cca181101b68569d0f64c2038b7c5ffb0ceb66fb
David Chen <dychen2523@gmail.com>
2024-01-12 17:44:29 -0500

    swerve code

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2024RobotCode";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "6238ea9fb85d7f0c41bd5dec8fe17cb125498b6d";
+  public static final String GIT_DATE = "2024-01-09 16:05:09 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-01-12 17:38:42 EST";
+  public static final long BUILD_UNIX_TIME = 1705099122187L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
