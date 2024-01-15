5f6f20d8840bc1ea544db5aef75c06044fcd541b
Vignesh1234587 <vigneshprabhakaran05@gmail.com>
2024-01-14 19:31:41 -0500

    added IO layers to shooter

--- src/main/java/frc/robot/subsystems/Shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIO.java
@@ -1,6 +1,20 @@
 package frc.robot.subsystems.Shooter;
 
 public interface ShooterIO {
-    
-    
+    //@AutoLog
+    public static class ShooterIOInputs{
+       public double shooterVelocity = 0;
+       public double appliedAmps = 0;
+       public double currentAmps = 0;
+
+    }
+
+    public default void updateInputs(ShooterIOInputs inputs) {}
+
+    public default void setVelocity(double velocity) {}
+
+    public default void stop() {}
+
+    public default void configurePID(double kP, double kI, double kD) {}
+
 }
---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIOTalonFX.java
@@ -0,0 +1,70 @@
+package frc.robot.subsystems.Shooter;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.util.Units;
+
+public class ShooterIOTalonFX implements ShooterIO{
+
+ private final TalonFX falcon; 
+ 
+ private final StatusSignal<Double> shooterVelocity;
+ private final StatusSignal<Double> appliedAmps;
+ private final StatusSignal<Double> currentAmps;
+ 
+    public ShooterIOTalonFX (int id){
+        
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.CurrentLimits.StatorCurrentLimit = 0;
+        config.CurrentLimits.StatorCurrentLimitEnable = true;
+        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        
+
+        falcon = new TalonFX(id);
+
+        falcon.getConfigurator().apply(config);
+
+        shooterVelocity = falcon.getVelocity();
+        appliedAmps = falcon.getMotorVoltage();
+        currentAmps = falcon.getStatorCurrent();
+
+        BaseStatusSignal.setUpdateFrequencyForAll(100, shooterVelocity, appliedAmps, currentAmps);
+
+    
+    }
+
+    @Override
+    public void updateInputs(ShooterIOInputs inputs) {
+        inputs.shooterVelocity = Units.rotationsPerMinuteToRadiansPerSecond(shooterVelocity.getValueAsDouble());
+
+        inputs.appliedAmps = appliedAmps.getValueAsDouble();
+
+        inputs.currentAmps = currentAmps.getValueAsDouble();
+
+    }
+    @Override
+    public void setVelocity(double velocity) {
+        falcon.setControl(new VelocityVoltage(velocity));
+
+    }
+    @Override
+    public void stop() {
+        falcon.stopMotor();
+    }
+    
+    @Override
+    public void configurePID(double kP, double kI, double kD) {
+        Slot0Configs configs = new Slot0Configs();
+
+        configs.kP = kP;
+        configs.kI = kI;
+        configs.kD = kD;
+
+        falcon.getConfigurator().apply(configs);
+    }
+}
