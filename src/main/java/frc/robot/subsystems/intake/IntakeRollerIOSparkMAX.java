34d7bf08499de695ab0b9a4e15ef6caa692e66ce
CSnair7 <149336161+CSnair7@users.noreply.github.com>
2024-01-16 19:50:18 -0500

    created intakeRollerIO and TalonFX and SparkMAX implementation

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeRollerIOSparkMAX.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems.intake;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
+
+public class IntakeRollerIOSparkMAX implements IntakeRollerIO{
+    private final CANSparkMax rollers;
+    private final SparkPIDController pid;
+
+    public IntakeRollerIOSparkMAX(int id) {
+        rollers = new CANSparkMax(id, MotorType.kBrushless);
+        rollers.restoreFactoryDefaults();
+
+        rollers.setSmartCurrentLimit(30);
+        rollers.setCANTimeout(250);
+        rollers.burnFlash();
+
+        pid = rollers.getPIDController();
+    }
+
+    @Override
+    public void updateInputs(IntakeRollerIOInputs inputs) {
+        inputs.rollerVelocity = rollers.getEncoder().getVelocity();
+
+        inputs.appliedVolts = rollers.getBusVoltage();
+        inputs.currentAmps = rollers.getOutputCurrent();
+    }
+
+    @Override
+    public void setVelocity(double velocity) {
+        pid.setReference(velocity, ControlType.kVelocity);
+    }
+
+    @Override
+    public void stop() {
+        rollers.stopMotor();
+    }
+
+    @Override
+    public void configurePID(double kP, double kI, double kD) {
+        pid.setP(kP, 0);
+        pid.setI(kI, 0);
+        pid.setD(kD, 0);
+    }
+}
