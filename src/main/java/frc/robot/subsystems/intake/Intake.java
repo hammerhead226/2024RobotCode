e42c5774001c7548c6ca579e87003dc3cfbae341
Vignesh1234587 <vigneshprabhakaran05@gmail.com>
2024-01-19 20:56:31 -0500

    intake subsystem finished

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -4,16 +4,52 @@
 
 package frc.robot.subsystems.intake;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
-  public Intake() {
-    
+  private final IntakePivotIO pivot;
+  private final IntakeRollerIO roller;
+  private final IntakePivotIOInputsAutoLogged pInputs = new IntakePivotIOInputsAutoLogged();
+  private final IntakeRollerIOInputsAutoLogged rInputs = new IntakeRollerIOInputsAutoLogged();
+
+  public Intake(IntakePivotIO pivot, IntakeRollerIO roller) {
+    this.pivot = pivot;
+    this.roller = roller;
+
   }
 
+
+  public void runRollers(double velocity){
+     roller.setVelocity(velocity);
+
+  }
+
+  public void stopRollers(){
+    roller.stop();
+  }
+
+  public void stopPivot(){
+
+    pivot.stop();
+  }
+
+  public void setPosition(double position){
+
+    pivot.setPosition(position);
+
+    Logger.recordOutput("Position ", position);
+  }
+  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    pivot.updateInputs(pInputs);
+    roller.updateInputs(rInputs);
+
+    Logger.processInputs("pivot Motor ", pInputs);
+    Logger.processInputs("roller Motor", rInputs);
   }
 }
