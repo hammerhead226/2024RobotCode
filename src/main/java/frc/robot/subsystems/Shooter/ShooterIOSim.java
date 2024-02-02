f43bc709fcf59297bf8b921cadbe0d903825b71c
CSnair7 <149336161+CSnair7@users.noreply.github.com>
2024-02-01 19:16:14 -0500

    created base IOSIm layer for shooter
    nothing is in it right now

---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterIOSim.java
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Shooter;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+
+/** Add your docs here. */
+public class ShooterIOSim {
+
+}
