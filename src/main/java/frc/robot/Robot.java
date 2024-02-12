247b65d2b12666c97755cbd3ab48704cd95c1a0f
AlpacianCodaa <abhinav.thummalapally@gmail.com>
2024-02-12 18:31:36 -0500

    USB Camera updated

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,6 +27,9 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+    UsbCamera camera = CameraServer.startAutomaticCapture(0);
+    camera.setResolution(400, 380);
+    camera.setExposureManual(50);
     m_robotContainer = new RobotContainer();
   }
 
