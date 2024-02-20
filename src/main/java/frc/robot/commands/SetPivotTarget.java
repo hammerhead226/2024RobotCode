51da0a62524db67a4b6e499b87cd8911a2d28e45
AlpacianCodaa <abhinav.thummalapally@gmail.com>
2024-02-20 18:05:06 -0500

    Moved command from execute to initialize

--- src/main/java/frc/robot/commands/SetPivotTarget.java
+++ src/main/java/frc/robot/commands/SetPivotTarget.java
@@ -38,13 +38,13 @@ public class SetPivotTarget extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    elevator.setPivotGoal(setPoint);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    elevator.setPivotGoal(setPoint);
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
