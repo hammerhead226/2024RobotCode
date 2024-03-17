cb3ff5eeee9ce7a090fdc5b45a2968bfac0ff609
David Chen <dychen2523@gmail.com>
2024-03-16 20:59:12 -0400

    2.5 amp side

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 166;
-  public static final String GIT_SHA = "02be8d9f8b56af0dc34243ed94e2dd7c070758f0";
-  public static final String GIT_DATE = "2024-03-16 19:01:37 EDT";
+  public static final int GIT_REVISION = 167;
+  public static final String GIT_SHA = "f765450f487c1e4f613540a4a0b4d6a49388db3e";
+  public static final String GIT_DATE = "2024-03-16 20:41:18 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-03-16 20:39:39 EDT";
-  public static final long BUILD_UNIX_TIME = 1710635979851L;
+  public static final String BUILD_DATE = "2024-03-16 20:57:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1710637066689L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
