f1820d119146d42f8e0c94a900538c6d85036647
David Chen <dychen2523@gmail.com>
2024-03-21 22:56:39 -0400

    3 note amp

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -389,7 +389,7 @@ public class RobotContainer {
         new SetPivotTarget(Constants.PivotConstants.SUBWOOFER_SETPOINT_DEG, pivot));
 
     // PIVOT NAMED COMMANDS (to speed up autoaim)
-    NamedCommands.registerCommand("Pivot40", new SetPivotTarget(40, pivot));
+    NamedCommands.registerCommand("Pivot39", new SetPivotTarget(39, pivot));
     NamedCommands.registerCommand("Pivot45", new SetPivotTarget(45, pivot));
     NamedCommands.registerCommand("Pivot50", new SetPivotTarget(50, pivot));
 
--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -193,12 +193,12 @@ public class Drive extends SubsystemBase {
     Logger.recordOutput("avg area", limelightMeasurement.avgTagArea);
 
     if (limelightMeasurement.tagCount >= 2 && limelightMeasurement.avgTagArea > 0.04) {
-      xMeterStds = 0.9;
-      yMeterStds = 0.9;
-      headingDegStds = 0.9;
+      xMeterStds = 0.7;
+      yMeterStds = 0.7;
+      headingDegStds = 8;
     } else if (limelightMeasurement.tagCount == 1 && poseDifference < 0.5) {
-      xMeterStds = 2;
-      yMeterStds = 2;
+      xMeterStds = 5;
+      yMeterStds = 5;
       headingDegStds = 30;
     } else if (limelightMeasurement.tagCount == 1 && poseDifference < 3) {
       xMeterStds = 11.43;
@@ -219,6 +219,7 @@ public class Drive extends SubsystemBase {
 
     Logger.recordOutput("Vision Measurement", limelightMeasurement.pose);
     Logger.recordOutput("limelilght latency", limelightMeasurement.latency);
+
     addVisionMeasurement(
         pose, limelightMeasurement.timestampSeconds - (limelightMeasurement.latency / 1000.));
   }
