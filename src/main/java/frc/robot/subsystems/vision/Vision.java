4956e6326d55bc396c4fc48a55f32c0206ae4be0
David Chen <dychen2523@gmail.com>
2024-03-27 12:18:33 -0400

    working on cam > bot > field coords

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -4,16 +4,37 @@
 
 package frc.robot.subsystems.vision;
 
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
   VisionIO io;
   VisionIOInputsAutoLogged inputs = new VisionIOInputsAutoLogged();
   /** Creates a new Vision. */
-  public Vision() {}
+  public Vision(VisionIO io) {
+    this.io = io;
+  }
+
+  public Translation2d getRobotRelNoteLocation(double gyro) {
+    double distance = inputs.distanceMeters;
+    double angle = inputs.angleDegs;
+
+    if (DriverStation.getAlliance().get().equals(Alliance.Red)) {
+      // if (gyro )
+    }
+
+    return new Translation2d();
+  }
 
   @Override
   public void periodic() {
+    io.updateInputs(inputs);
+
+    Logger.processInputs("Vision", inputs);
     // This method will be called once per scheduler run
   }
 }
