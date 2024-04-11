f2854b770ca4fc01eb56456e306757c90012878a
David Chen <dychen2523@gmail.com>
2024-04-10 20:52:43 -0400

    speed up!

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -121,6 +121,8 @@ public final class Constants {
     public static final double CLIMB_SETPOINT_ONE_DEG = 50;
     public static final double CLIMB_SETPOINT_TWO_DEG = 95;
 
+    public static final double TRAP_SETPOINT_DEG = 75;
+
     public static final double THRESHOLD = 1.5;
 
     public static final double[] PID = {0, 0, 0};
