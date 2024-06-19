32e2da7bc63ca432ebb9ccf2a9d5694c6acec948
David Chen <dychen2523@gmail.com>
2024-06-19 14:33:05 -0400

    removing the check

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 265;
-  public static final String GIT_SHA = "250c8f424e36d3ccca4482975d388b98de16cac9";
-  public static final String GIT_DATE = "2024-06-13 21:27:39 EDT";
+  public static final int GIT_REVISION = 266;
+  public static final String GIT_SHA = "fa8c6538ac615817ee57abf0dbc507f77bfa092f";
+  public static final String GIT_DATE = "2024-06-14 00:38:29 EDT";
   public static final String GIT_BRANCH = "demo";
-  public static final String BUILD_DATE = "2024-06-14 00:37:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1718339866304L;
+  public static final String BUILD_DATE = "2024-06-14 17:21:28 EDT";
+  public static final long BUILD_UNIX_TIME = 1718400088751L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
