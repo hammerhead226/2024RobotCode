b1b6e3205d31d1312f8405f3bbea5539a3c4c823
David Chen <dychen2523@gmail.com>
2024-07-08 23:36:46 -0400

    changed pth

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 282;
-  public static final String GIT_SHA = "3ff066b9a271835eaab658c5ac7cb29726eaf7ce";
-  public static final String GIT_DATE = "2024-07-08 16:18:13 EDT";
+  public static final int GIT_REVISION = 283;
+  public static final String GIT_SHA = "1bbe432c459d5a47ba7632f4ecd21b430021a73d";
+  public static final String GIT_DATE = "2024-07-08 19:55:42 EDT";
   public static final String GIT_BRANCH = "vision-subsytem";
-  public static final String BUILD_DATE = "2024-07-08 19:52:27 EDT";
-  public static final long BUILD_UNIX_TIME = 1720482747342L;
+  public static final String BUILD_DATE = "2024-07-08 20:00:53 EDT";
+  public static final long BUILD_UNIX_TIME = 1720483253403L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
