03163e6d6538ff0f16142eb570143592edacb144
Vignesh1234587 <vigneshprabhakaran05@gmail.com>
2024-07-25 09:42:01 -0400

    changed aimbot static p gain back

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024 Robot Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 301;
-  public static final String GIT_SHA = "0e925a72ad8375d482624b6109dc7195fc5c78ec";
-  public static final String GIT_DATE = "2024-07-22 21:10:19 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-07-24 20:16:45 EDT";
-  public static final long BUILD_UNIX_TIME = 1721866605799L;
+  public static final int GIT_REVISION = 302;
+  public static final String GIT_SHA = "aa68d3ebe1c28e623378ea14d42692379d2f130e";
+  public static final String GIT_DATE = "2024-07-24 20:27:42 EDT";
+  public static final String GIT_BRANCH = "pre-RR";
+  public static final String BUILD_DATE = "2024-07-24 20:45:18 EDT";
+  public static final long BUILD_UNIX_TIME = 1721868318331L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
