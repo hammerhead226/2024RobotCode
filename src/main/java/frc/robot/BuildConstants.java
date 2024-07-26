e738e17eb7258745aec51c5deff061d31c4bce88
David Chen <dychen2523@gmail.com>
2024-07-26 19:17:58 -0400

    dont remember

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 303;
-  public static final String GIT_SHA = "71124a99d7d62665f20d295b5ec32ed967663ef4";
-  public static final String GIT_DATE = "2024-07-26 18:55:57 EDT";
+  public static final int GIT_REVISION = 304;
+  public static final String GIT_SHA = "5bc910513e7bcfd602293d83b68b6e352918016c";
+  public static final String GIT_DATE = "2024-07-26 18:58:28 EDT";
   public static final String GIT_BRANCH = "rainbow-rumble";
-  public static final String BUILD_DATE = "2024-07-26 18:57:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1722034666677L;
+  public static final String BUILD_DATE = "2024-07-26 18:59:58 EDT";
+  public static final long BUILD_UNIX_TIME = 1722034798371L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
