e76e9a3ddf3e4bc4bc7f0c7c82a97b8b09d71b99
devinisaperson <177495008+devinisaperson@users.noreply.github.com>
2025-08-20 14:46:58 -0400

    patched with 2025RobotCode gradle and venderdeps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 357;
-  public static final String GIT_SHA = "3ef842811300f6ae93f92b9acc1813edc4af7f2f";
-  public static final String GIT_DATE = "2024-10-19 17:38:18 EDT";
-  public static final String GIT_BRANCH = "bloomfield";
-  public static final String BUILD_DATE = "2024-10-19 18:17:32 EDT";
-  public static final long BUILD_UNIX_TIME = 1729376252676L;
+  public static final int GIT_REVISION = 358;
+  public static final String GIT_SHA = "9dfa53aa84b5d502fa0383ae05141e56a77b6c3a";
+  public static final String GIT_DATE = "2024-10-20 11:22:43 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-08-20 14:44:31 EDT";
+  public static final long BUILD_UNIX_TIME = 1755715471334L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
