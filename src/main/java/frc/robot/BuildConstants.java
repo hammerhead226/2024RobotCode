d7f96d4fecde00bf4c61289e874416e1ab9a7cb2
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-10-04 12:09:37 -0400

    Fixed Littletronorg connection issue
    Fixed Littletronorg connection issue

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 357;
-  public static final String GIT_SHA = "3ef842811300f6ae93f92b9acc1813edc4af7f2f";
-  public static final String GIT_DATE = "2024-10-19 17:38:18 EDT";
-  public static final String GIT_BRANCH = "bloomfield";
-  public static final String BUILD_DATE = "2024-10-19 18:17:32 EDT";
-  public static final long BUILD_UNIX_TIME = 1729376252676L;
+  public static final int GIT_REVISION = 358;
+  public static final String GIT_SHA = "9dfa53aa84b5d502fa0383ae05141e56a77b6c3a";
+  public static final String GIT_DATE = "2024-10-20 11:22:43 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-10-04 12:08:01 EDT";
+  public static final long BUILD_UNIX_TIME = 1759594081854L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
